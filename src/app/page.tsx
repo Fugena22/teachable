0241c7ec72653439cf692b66be4d6c8302389d2c
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-03-25 14:49:36 +0700

    change-link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex justify-center items-center">
-      <Link href="https://sso.teachable.com/secure/2026611/identity/oauth_provider/authorize?client_id=WZC1EmoH3uEjb4EaAgXA5l0xnCeVOMqc&response_type=code&required_scopes=name:read%20email:read&optional_scopes=courses:read">
+      <Link href="https://sso.teachable.com/secure/2026611/identity/oauth_provider/authorize?client_id=NABnQHrCV13tix15hoQ1o0jzYK720JYW&response_type=code&required_scopes=name:read%20email:read&optional_scopes=courses:read">
         <Button>Login</Button>
       </Link>
     </main>
